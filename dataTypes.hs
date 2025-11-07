e6dd679201e122d9e6bc122c778cac47a734141d
mconner2 <mconner2@trinity.edu>
2025-11-07 14:37:16 -0600

    added a little more

--- dataTypes.hs
+++ dataTypes.hs
@@ -2,5 +2,16 @@ data Spot = Player Symbol | Empty deriving (Show, Eq) -- maybe ord?
 data Board = Incomplete [Spot] | Complete Spot deriving (Show, Eq)
 data Symbol = X | O deriving (Show, Eq)
 
+nextPlayer :: player -> player
+nextPlayer X = O
+nextPlayer O = X
+
+
+gameWinner :: Board -> Player
+--Checks the Board, returns Empty if game is a draw, Cont if Game is still continuing, X or O for if respective player won
+nextPlayer :: Player -> Player
+nextPlayer X = O
+nextPlayer O = X
+
 -- obviously this is just some of my code, you guys feel free to choose what you want from our doc
 -- i just put mine in as a placeholder
