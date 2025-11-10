ed7d744433a17e224091c91d02d458414a40eb95
mconner2 <mconner2@trinity.edu>
2025-11-10 15:07:38 -0600

    shows basic board

--- dataTypes.hs
+++ dataTypes.hs
@@ -28,6 +28,38 @@ nextPlayer O = X
 
 -- prettyPrint :: GameState -> String
     
+-- tester board
+-- subBoard1 = Incomplete [Full X, Full O, Emp, Full X, Full O, Emp, Full O, Full X, Emp]
+-- subBoard2 = Complete $ Won X
+-- board = [subBoard1, subBoard1, subBoard 2, subBoard1, subBoard 1, subBoard 2, subBoard 2, subBoard 2, subBoard 1]
+subBoardStr :: SubBoard -> (String, String, String)
+subBoardStr (Complete w) = 
+    ("           ", 
+    "     " ++ winState ++ "     ",
+    "           ")
+  where
+    winState = case w of
+        Won X       -> "X"
+        Won O       -> "O"
+        Tie         -> "T"
+        Unfinished  -> " "
+subBoardStr (Incomplete lst) =
+    let rows = chunksOf 3 (map showSpot lst)
+        rowStrs = map (intercalate "|") rows
+    in
+      case rowStrs of
+        [r1, r2, r3] -> (r1, r2, r3)
+
+printMainBoard :: Board -> String
+printMainBoard board =
+  let subStrs = map subBoardStr board
+      mainRows = chunksOf 3 subStrs
+      totalRow row =
+        let (r1s, r2s, r3s) = unzip3 row
+        in unlines [intercalate "||" r1s, intercalate "||" r2s, intercalate "||" r3s]
+  in intercalate "=====================================" $ map totalRow mainRows
+
+
 
 printSubBoard :: SubBoard -> String
 printSubBoard (Complete w) = 
