0bd13496e1e93e1f7a5e7833ec7b00076322220f
mconner2 <mconner2@trinity.edu>
2025-11-14 14:29:14 -0600

    light formatting, moved stuff around

--- dataTypes.hs
+++ dataTypes.hs
@@ -4,40 +4,103 @@
 import Data.List (intercalate)
 import Data.List.Split (chunksOf)
 
-type Location = Int
+-- DEFINE ALL DATA TYPES
 allLocations = [0..8]
+type Location = Int
+
+data Player = X | O deriving (Show, Eq)
 data Spot = Full Player | Emp deriving (Show, Eq)
+data Winner = Won Player | Tie | Unfinished deriving (Show, Eq)
 data SubBoard = Incomplete [Spot] | Complete Winner deriving (Show, Eq)
 type Board = [SubBoard]
-data Player = X | O deriving (Show, Eq)
-data Winner = Won Player | Tie | Unfinished deriving (Show, Eq)
-type GameState = (Board, Player, Maybe Location)
+
 type Move = (Location, Location)
+type GameState = (Board, Player, Maybe Location)
 
+-- DEFINE FUNCTIONS FOR DATA TYPES
+isPlayerX :: (Location, Spot) -> Maybe Location
+isPlayerX (i, Full X) = Just i
+isPlayerX _ = Nothing
+isPlayerO :: (Location, Spot) -> Maybe Location
+isPlayerO (i, Full O) = Just i
+isPlayerO _ = Nothing
 
--- nextPlayer :: player -> player
--- nextPlayer X = O
--- nextPlayer O = X
-
-
--- gameWinner :: Board -> Player
 nextPlayer :: Player -> Player
 nextPlayer X = O
 nextPlayer O = X
 
+-- PRINTING BOARD FUNCS
+-- tester board
+-- subBoard1 = Incomplete [Full X, Full O, Emp, Full X, Full O, Emp, Full O, Full X, Emp]
+-- subBoard2 = Complete $ Won X
+-- board = [subBoard1, subBoard1, subBoard 2, subBoard1, subBoard 1, subBoard 2, subBoard 2, subBoard 2, subBoard 1]
+subBoardStr :: SubBoard -> (String, String, String)
+subBoardStr (Complete w) = 
+    ("           ", 
+    "     " ++ winState ++ "     ",
+    "           ")
+  where
+    winState = case w of
+        Won X       -> "X"
+        Won O       -> "O"
+        Tie         -> "T"
+        Unfinished  -> " "
+subBoardStr (Incomplete lst) =
+    let rows = chunksOf 3 (map showSpot lst)
+        rowStrs = map (intercalate "|") rows
+    in
+      case rowStrs of
+        [r1, r2, r3] -> (r1, r2, r3)
 
-gameWinner :: Board -> Winner
-gameWinner brd = let temp = (boardToSubBoard brd)
-                 in subBoardWinner temp
-                     
+printMainBoard :: Board -> String
+printMainBoard board =
+  let subStrs = map subBoardStr board
+      mainRows = chunksOf 3 subStrs
+      totalRow row =
+        let (r1s, r2s, r3s) = unzip3 row
+        in unlines [intercalate "||" r1s, intercalate "||" r2s, intercalate "||" r3s]
+  in intercalate "=====================================" $ map totalRow mainRows
 
-isPlayerX :: (Location, Spot) -> Maybe Location
-isPlayerX (i, Full X) = Just i
-isPlayerX _ = Nothing
 
-isPlayerO :: (Location, Spot) -> Maybe Location
-isPlayerO (i, Full O) = Just i
-isPlayerO _ = Nothing
+
+printSubBoard :: SubBoard -> String
+printSubBoard (Complete w) = 
+    "           \n" ++
+    "     " ++ winState ++ "     \n" ++
+    "           \n"
+  where
+    winState = case w of
+        Won X       -> "X"
+        Won O       -> "O"
+        Tie         -> "T"
+        Unfinished  -> " "
+        
+printSubBoard (Incomplete lst) =
+    let rows = chunksOf 3 (map showSpot lst)
+    in unlines [intercalate "|" r | r <- rows]
+
+showSpot :: Spot -> String
+showSpot (Full X) = " X "
+showSpot (Full O) = " O "
+showSpot Emp      = "   "
+
+-- GAME FUNCS
+checkWinner [Location] -> Bool
+checkWinner xs = let
+  b1 = checkVertical 0 xs   || checkVertical 1 xs   || checkVertical 2 xs
+  b2 = checkHorizontal 0 xs || checkHorizontal 3 xs || checkHorizontal 6 xs
+  b3 = checkDiagonalLeft xs || checkDiagonalRight xs
+  in b1 || b2 || b3
+
+checkVertical :: Location -> [Location] -> Bool
+checkVertical a xs = (a `elem` xs) && ((a+3) `elem` xs) && $ (a+6) `elem` xs 
+checkHorizontal :: Location -> [Location] -> Bool
+checkHorizontal a xs = (a `elem` xs) && ((a+1) `elem` xs) && $ (a+2) `elem xs
+checkDiagonalLeft :: [Location] -> Bool
+checkDiagonalLeft xs = (0 `elem` xs) && (4 `elem` xs) && (8 `elem` xs)
+checkDiagonalRight :: [Location] -> Bool
+checkDiagonalRight xs = (2 `elem` xs) && (4 `elem` xs) && (6 `elem` xs)
+
 
 subBoardWinner:: SubBoard -> Winner
 subBoardWinner Complete a = a
@@ -55,21 +118,9 @@ subBoardWinner Incomplete spots =
                  else if boardFull
                       then Tie
                       else Unfinished
-
-checkVertical :: Location -> [Location] -> Bool
-checkVertical a xs = (a `elem` xs) && ((a+3) `elem` xs) && $ (a+6) `elem` xs 
-checkHorizontal :: Location -> [Location] -> Bool
-checkHorizontal a xs = (a `elem` xs) && ((a+1) `elem` xs) && $ (a+2) `elem xs
-checkDiagonalLeft :: [Location] -> Bool
-checkDiagonalLeft xs = (0 `elem` xs) && (4 `elem` xs) && (8 `elem` xs)
-checkDiagonalRight :: [Location] -> Bool
-checkDiagonalRight xs = (2 `elem` xs) && (4 `elem` xs) && (6 `elem` xs)
-
-checkWinner [Location] -> Bool
-checkWinner xs = let b1 = checkVertical 0 xs   || checkVertical 1 xs   || checkVertical 2 xs
-                     b2 = checkHorizontal 0 xs || checkHorizontal 3 xs || checkHorizontal 6 xs
-                     b3 = checkDiagonalLeft xs || checkDiagonalRight xs
-                     in b1 || b2 || b3
+gameWinner :: Board -> Winner
+gameWinner brd = let temp = (boardToSubBoard brd)
+                 in subBoardWinner temp
 
 
 boardToSubBoard :: Board -> [Spot]
@@ -77,133 +128,16 @@ boardToSubBoard [] = []
 boardToSubBoard (Complete Player a:xs) =(Full a):(aux xs)
 boardToSubBoard (x:xs) = Emp:xs
 
--- prettyPrint :: GameState -> String
-    
--- tester board
--- subBoard1 = Incomplete [Full X, Full O, Emp, Full X, Full O, Emp, Full O, Full X, Emp]
--- subBoard2 = Complete $ Won X
--- board = [subBoard1, subBoard1, subBoard 2, subBoard1, subBoard 1, subBoard 2, subBoard 2, subBoard 2, subBoard 1]
-subBoardStr :: SubBoard -> (String, String, String)
-subBoardStr (Complete w) = 
-    ("           ", 
-    "     " ++ winState ++ "     ",
-    "           ")
-  where
-    winState = case w of
-        Won X       -> "X"
-        Won O       -> "O"
-        Tie         -> "T"
-        Unfinished  -> " "
-subBoardStr (Incomplete lst) =
-    let rows = chunksOf 3 (map showSpot lst)
-        rowStrs = map (intercalate "|") rows
-    in
-      case rowStrs of
-        [r1, r2, r3] -> (r1, r2, r3)
-
-printMainBoard :: Board -> String
-printMainBoard board =
-  let subStrs = map subBoardStr board
-      mainRows = chunksOf 3 subStrs
-      totalRow row =
-        let (r1s, r2s, r3s) = unzip3 row
-        in unlines [intercalate "||" r1s, intercalate "||" r2s, intercalate "||" r3s]
-  in intercalate "=====================================" $ map totalRow mainRows
-
-
-
-printSubBoard :: SubBoard -> String
-printSubBoard (Complete w) = 
-    "           \n" ++
-    "     " ++ winState ++ "     \n" ++
-    "           \n"
-  where
-    winState = case w of
-        Won X       -> "X"
-        Won O       -> "O"
-        Tie         -> "T"
-        Unfinished  -> " "
-        
-printSubBoard (Incomplete lst) =
-    let rows = chunksOf 3 (map showSpot lst)
-    in unlines [intercalate "|" r | r <- rows]
-
-showSpot :: Spot -> String
-showSpot (Full X) = " X "
-showSpot (Full O) = " O "
-showSpot Emp      = "   "
-
--- prettyPrint :: GameState -> String
-    
--- tester board
--- subBoard1 = Incomplete [Full X, Full O, Emp, Full X, Full O, Emp, Full O, Full X, Emp]
--- subBoard2 = Complete $ Won X
--- board = [subBoard1, subBoard1, subBoard 2, subBoard1, subBoard 1, subBoard 2, subBoard 2, subBoard 2, subBoard 1]
-subBoardStr :: SubBoard -> (String, String, String)
-subBoardStr (Complete w) = 
-    ("           ", 
-    "     " ++ winState ++ "     ",
-    "           ")
-  where
-    winState = case w of
-        Won X       -> "X"
-        Won O       -> "O"
-        Tie         -> "T"
-        Unfinished  -> " "
-subBoardStr (Incomplete lst) =
-    let rows = chunksOf 3 (map showSpot lst)
-        rowStrs = map (intercalate "|") rows
-    in
-      case rowStrs of
-        [r1, r2, r3] -> (r1, r2, r3)
-
-printMainBoard :: Board -> String
-printMainBoard board =
-  let subStrs = map subBoardStr board
-      mainRows = chunksOf 3 subStrs
-      totalRow row =
-        let (r1s, r2s, r3s) = unzip3 row
-        in unlines [intercalate "||" r1s, intercalate "||" r2s, intercalate "||" r3s]
-  in intercalate "=====================================" $ map totalRow mainRows
-
-
-
-printSubBoard :: SubBoard -> String
-printSubBoard (Complete w) = 
-    "           \n" ++
-    "     " ++ winState ++ "     \n" ++
-    "           \n"
-  where
-    winState = case w of
-        Won X       -> "X"
-        Won O       -> "O"
-        Tie         -> "T"
-        Unfinished  -> " "
-        
-printSubBoard (Incomplete lst) =
-    let rows = chunksOf 3 (map showSpot lst)
-    in unlines [intercalate "|" r | r <- rows]
-
-showSpot :: Spot -> String
-showSpot (Full X) = " X "
-showSpot (Full O) = " O "
-showSpot Emp      = "   "
-
--- gameWinner :: Board -> Player
---Checks the Board, returns Empty if game is a draw, Cont if Game is still continuing, X or O for if respective player won
-
--- i am going insane please give me a for loop haskell
 checkLegalMoves :: Board -> [Move]
 checkLegalMoves game = let -- auxMain is a recursive function that goes through a board and calls auxSub on all incomplete boards (boards with legal moves).
-                           auxMain _ [] = []
-                           auxMain boardLoc ((Incomplete sub):xs) = auxSub boardLoc 0 sub ++ auxMain (boardLoc + 1) xs
-                           auxMain boardLoc ((Complete _):xs) = auxMain (boardLoc + 1) xs
-                           -- auxSub is a recursive function that goes through a subboard and finds all the legal moves in the board, returning legal moves as a Move.
-                           auxSub _ _ [] = []
-                           auxSub boardLoc subBoardLoc (Emp:xs) = (boardLoc, subBoardLoc):auxSub boardLoc (subBoardLoc + 1) xs
-                           auxSub boardLoc subBoardLoc ((Full _):xs) = auxSub boardLoc (subBoardLoc + 1) xs
-                       in  auxMain 0 game
-
+  auxMain _ [] = []
+  auxMain boardLoc ((Incomplete sub):xs) = auxSub boardLoc 0 sub ++ auxMain (boardLoc + 1) xs
+  auxMain boardLoc ((Complete _):xs) = auxMain (boardLoc + 1) xs
+  -- auxSub is a recursive function that goes through a subboard and finds all the legal moves in the board, returning legal moves as a Move.
+  auxSub _ _ [] = []
+  auxSub boardLoc subBoardLoc (Emp:xs) = (boardLoc, subBoardLoc):auxSub boardLoc (subBoardLoc + 1) xs
+  auxSub boardLoc subBoardLoc ((Full _):xs) = auxSub boardLoc (subBoardLoc + 1) xs
+  in auxMain 0 game
 
 -- Place a player's mark in a given spot of a sub-board
 placeSpot :: Player -> SubBoard -> Location -> SubBoard
@@ -214,7 +148,6 @@ placeSpot p (Incomplete spots) loc =
        Unfinished -> Incomplete newSpots
        _          -> Complete winner
 placeSpot _ board@(Complete _) _ = board  -- no change if already complete
-
 
 -- Check if a sub-board has a winner or tie
 checkSubBoard :: [Spot] -> Winner
@@ -252,7 +185,6 @@ checkOverall board =
   where
     subToWinner (Complete w) = w
     subToWinner _            = Unfinished
-
 
 -- Core function: make a legal move
 makeMove :: GameState -> Move -> GameState
