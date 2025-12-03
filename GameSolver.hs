1edb0cde0846440e5f183329fbb9dd6eea071302
Natalie Hudson~ <nhudson@trinity.edu>
2025-12-03 14:05:13 -0600

    bestMove slight improvement

--- GameSolver.hs
+++ GameSolver.hs
@@ -6,9 +6,9 @@ import GamePrint
 import InputText
 
 bestMove :: GameState -> Move
-bestMove (game, currPlayer, l) = let
-    legalMoves = checkLegalMoves (game, currPlayer, l)
-    resultingGames = [(whoWillWin (makeMove (game, currPlayer, l) move), move) | move <- legalMoves]
+bestMove state@(game, currPlayer, _) = let
+    legalMoves = checkLegalMoves state
+    resultingGames = [(whoWillWin (makeMove state move), move) | move <- legalMoves]
     -- resultingGames is a list of tuples of the form (winner of the game resulting from the move being played, move)
     winningGame = lookup (Won currPlayer) resultingGames
     in case winningGame of
