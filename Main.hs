78e7677c27e75da3d0578840e747ecd7115d2632
Natalie Hudson~ <nhudson@trinity.edu>
2025-12-03 15:52:57 -0600

    flag data + options

--- Main.hs
+++ Main.hs
@@ -4,6 +4,19 @@ import GameCode
 import GameText
 import GameSolver
 import System.IO
+import System.Console.GetOpt
+
+data Flag = Help | Winner | Depth String | Move Move | Verbose-- | Interactive
+
+options :: [OptDescr Flag]
+options = [ Option ['h'] ["help"] (NoArg Help) "Print usage information and exit."
+          , Option ['w'] ["winner"] (NoArg Winner) "Find the best move for the current player."
+          , Option ['d'] ["depth"] (ReqArg Depth "<#>") "Find the best move for the current player with a specified cutoff depth."
+          , Option ['m'] ["move"] (ReqArg (Move . read) "<move>") "Make the specified move and print the resulting board."
+          , Option ['v'] ["verbose"] (NoArg Verbose) "An option for -move; print the resulting board alongside a rating of the move made."
+          -- , Option ['i'] ["interactive"] (NoArg Interactive) "Start a new game and play against the computer."
+          ]
+          
 -- other io
 putBestMove :: GameState -> IO ()
 putBestMove state = putStrLn $ showMove $ bestMove state
