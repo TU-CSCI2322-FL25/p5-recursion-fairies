8ebbae73a24ce6ced25e58e3fa1d76f2af9990cd
mconner2 <mconner2@trinity.edu>
2025-12-03 14:41:49 -0600

    io improved

--- IOModule.hs
+++ IOModule.hs
@@ -2,6 +2,7 @@ module IOModule where
 import DataTypes
 import GameCode
 import GameText
+import GameSolver
 import System.IO
 -- other io
 putBestMove :: GameState -> IO ()
@@ -10,3 +11,13 @@ putBestMove state = putStrLn $ printGame stateAfter
 
 writeGameState :: FilePath -> GameState -> IO ()
 writeGameState path state = writeFile path $ gameStateOut state
+
+loadGameState :: FilePath -> IO GameState
+loadGameState = readGameState
+
+main :: IO ()
+main = do
+    putStrLn "What is filepath of gameState?"
+    path <- getLine
+    state <- loadGameState path
+    putBestMove state
